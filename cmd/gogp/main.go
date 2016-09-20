ad8dc0029c8608e4bdbfd15cbee77aa5a9816afe
Ally Dale <vipally@163.com>
2016-09-20 22:29:33 +0800

    refresh examples

--- cmd/gogp/main.go
+++ cmd/gogp/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	//"fmt"
 	"strings"
 
 	"github.com/vipally/cmdline"
@@ -10,12 +11,15 @@ import (
 
 var (
 	copyRightCode = "//    " + strings.Replace(cpright.CopyRight(), "\n", "\n//", strings.Count(cpright.CopyRight(), "\n")-1)
+	version       = "1.0.2"
 )
 
 func main() {
 	workPath := ""
 	exit_code := 0
 
+	cmdline.Version(version)
+	cmdline.CopyRight(cpright.CopyRight())
 	cmdline.Summary("Tool <thiscmd> is used to generate Generic-Programming code")
 	cmdline.StringVar(&workPath, "", "workPath", workPath, false, "path of gp")
 	cmdline.Parse()
@@ -23,6 +27,8 @@ func main() {
 	if workPath == "" {
 		workPath = cmdline.WorkDir()
 	}
+	copyRightCode = cmdline.ReplaceTags(copyRightCode)
+	gogp.CopyRight(copyRightCode)
 	gogp.Work(workPath)
 
 	//fmt.Println(copyRightCode)
