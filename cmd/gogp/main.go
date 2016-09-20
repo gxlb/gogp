962be8e359ce048c358856c36a785a41c15b50c9
Ally Dale <vipally@163.com>
2016-09-20 17:30:47 +0800

    to make gogp a library

---
+++ cmd/gogp/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/vipally/cmdline"
+	"github.com/vipally/cpright"
+	"github.com/vipally/gogp"
+)
+
+var (
+	copyRightCode = "//    " + strings.Replace(cpright.CopyRight(), "\n", "\n//", strings.Count(cpright.CopyRight(), "\n")-1)
+)
+
+func main() {
+	workPath := ""
+	exit_code := 0
+
+	cmdline.Summary("Tool <thiscmd> is used to generate Generic-Programming code")
+	cmdline.StringVar(&workPath, "", "path", workPath, false, "path of gp")
+	cmdline.Parse()
+
+	if workPath == "" {
+		workPath = cmdline.WorkDir()
+	}
+	gogp.Work(workPath)
+
+	//fmt.Println(copyRightCode)
+	//fmt.Println(cmdline.GetUsage())
+
+	cmdline.Exit(exit_code)
+}
