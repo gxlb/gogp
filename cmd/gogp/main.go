37fe36b140e11389455498f92d69307652cdbd32
Ally Dale <vipally@163.com>
2016-09-21 18:11:24 +0800

    update file head

--- cmd/gogp/main.go
+++ cmd/gogp/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"github.com/vipally/cmdline"
+	"github.com/vipally/cpright"
 	"github.com/vipally/gogp"
 )
 
 func main() {
 	workPath := ""
 	exit_code := 0
+
+	cmdline.Version(gogp.Version())
+	cmdline.CopyRight(cpright.CopyRight())
 
 	cmdline.Summary("Tool <thiscmd> is used to generate Generic-Programming code")
 	cmdline.StringVar(&workPath, "", "workPath", workPath, false, "path of gp")
