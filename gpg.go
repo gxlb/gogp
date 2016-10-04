56c02973363f1d29f82c8c3cce210781976e57b4
Ally Dale <vipally@163.com>
2016-10-05 00:11:46 +0800

    comment

--- gpg.go
+++ gpg.go
@@ -7,63 +7,63 @@
 package gogp is a generic-programming solution for golang or any other languages.
 
 usage of gogp tool:
-    1. (Recommend)use cmdline(cmd/gogp):
+  1. (Recommend)use cmdline(cmd/gogp):
 
-        Tool gogp is a generic-programming solution for golang or any other languages.
-		Usage:
-    		gogp [-r=<reverseWork>] <filePath>]
-		-e=<codeExt>  string
-    		Code file ext name. [.go] is default. [.gp] and [.gpg] is not allowed.
-		-r=<reverseWork>
-      		Reverse work, this mode is used to gen .gp file from a real-go file.
-      		If set this flag, the filePath flag must be a .gpg file path related to GoPath.
-  		<filePath>  string
-      		Path that gogp will work. GoPath and WorkPath is allowed.
+    Tool gogp is a generic-programming solution for golang or any other languages.
+    Usage:
+        gogp [-r=<reverseWork>] <filePath>]
+    -e=<codeExt>  string
+        Code file ext name. [.go] is default. [.gp] and [.gpg] is not allowed.
+    -r=<reverseWork>
+        Reverse work, this mode is used to gen .gp file from a real-go file.
+        If set this flag, the filePath flag must be a .gpg file path related to GoPath.
+    <filePath>  string
+        Path that gogp will work. GoPath and WorkPath is allowed.
 
-		Detail desctription:
-		1. .gpg files
-			An ini file in fact.It's used to define generic parameters's replacing relation.
-			Corresponding .gp file may with the same path and name, but we can redirect it by key "GOGP_GpFilePath".
-			Section "GOGP_REVERSE" is defined for ReverseWork mode to auto-generate .gp file from .go file.
-			So normal work mode will not generate go code file for this section.
+    Detail desctription:
+    1. .gpg files
+      An ini file in fact.It's used to define generic parameters's replacing relation.
+      Corresponding .gp file may with the same path and name, but we can redirect it by key "GOGP_GpFilePath".
+      Section "GOGP_REVERSE" is defined for ReverseWork mode to auto-generate .gp file from .go file.
+      So normal work mode will not generate go code file for this section.
 
-		2. .gp files
-			A go-like file, but exists some <xxx> format keys, need to be replaced with which defined in .gpg file.
+    2. .gp files
+      A go-like file, but exists some <xxx> format keys, need to be replaced with which defined in .gpg file.
 
-		3. .go files
-			gogp tool auto-generated .go files can be identification and compiled as well as normal go code files.
-			But never modify it manually, you can see this warning at the first line in every file.
-			Auto work on GoPath is recmmended.
-			gogp tool will deep travel the path to find all .gpg files to generate go code files for them.
-			If the generated go code file's body has no changes, this file will not be updated.
-			So run gogp tool any times on GoPath is harmless, unless there are indeed changes.
-			So any manually modification will be restored by this tool.
-			Take care of that.
+    3. .go files
+      gogp tool auto-generated .go files can be identification and compiled as well as normal go code files.
+      But never modify it manually, you can see this warning at the first line in every file.
+      Auto work on GoPath is recmmended.
+      gogp tool will deep travel the path to find all .gpg files to generate go code files for them.
+      If the generated go code file's body has no changes, this file will not be updated.
+      So run gogp tool any times on GoPath is harmless, unless there are indeed changes.
+      So any manually modification will be restored by this tool.
+      Take care of that.
 
-    2. package usage:
+  2. package usage:
 
-		2.1 (Recommend)import gogp package in test file
-	 		import (
-	 			//"testing"
-				"github.com/vipally/gogp"
-	 		)
-			func init() {
-				gogp.WorkOnGoPath() //Recommend
-				//gogp.WorkOnWorkPath()
-				//gogp.Work(someDir)
-				//gogp.ReverseWork("github.com/vipally/gogp/examples/reverse.gpg")
-			}
+    2.1 (Recommend)import gogp package in test file
+      import (
+          //"testing"
+          "github.com/vipally/gogp"
+      )
+      func init() {
+          gogp.WorkOnGoPath() //Recommend
+          //gogp.WorkOnWorkPath()
+          //gogp.Work(someDir)
+          //gogp.ReverseWork("github.com/vipally/gogp/examples/reverse.gpg")
+      }
 
-		2.2 (Seldom use)import gogp package in normal package
-			import (
-				"github.com/vipally/gogp"
-			)
-			func someFunc(){
-				//gogp.WorkOnGoPath()
-				//gogp.ReverseWork(gpgFilePath)
-				//gogp.WorkOnWorkPath()
-				//gogp.Work(someDir)
-			}
+    2.2 (Seldom use)import gogp package in normal package
+      import (
+          "github.com/vipally/gogp"
+      )
+      func someFunc(){
+          //gogp.WorkOnGoPath()
+          //gogp.ReverseWork(gpgFilePath)
+          //gogp.WorkOnWorkPath()
+          //gogp.Work(someDir)
+      }
 */
 package gogp
 
