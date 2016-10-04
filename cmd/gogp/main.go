deaa41e3c0705f1cd78e9a3f7a8bda6d9abfd38c
Ally Dale <vipally@163.com>
2016-10-04 22:50:35 +0800

    comment

--- cmd/gogp/main.go
+++ cmd/gogp/main.go
@@ -1,3 +1,4 @@
+//this is a cmdline interface of gogp tool.
 package main
 
 import (
@@ -15,7 +16,7 @@ func main() {
 	cmdline.Version(gogp.Version())
 	cmdline.CopyRight(cpright.CopyRight())
 
-	cmdline.Summary("Tool <thiscmd> is used to generate Generic-Programming code")
+	cmdline.Summary("Tool <thiscmd> is a generic-programming solution for golang or any other languages.")
 	cmdline.Details(`1. .gpg files
 		Is an ini file in fact.It's used to define generic parameters's replacing relation.
 		Corresponding .gp file may with the same path and name, but we can redirect it by key "GOGP_GpFilePath".
@@ -23,7 +24,7 @@ func main() {
 		So normal work mode will not generate go code file for this section.
 		
 	2. .gp files
-		Is a go-like file, but exists some <xxx> format keys, need to replace with which defined in .gpg file.
+		Is a go-like file, but exists some <xxx> format keys, need to be replaced with which are defined in .gpg file.
 		
 	3. .go files
 		gogp tool auto-generated .go files can be identification and compiled as well as normal go code files.
@@ -32,10 +33,11 @@ func main() {
 		gogp tool will deep travel the path to find all .gpg files to generate go code files for them.
 		If the generated go code file's body has no changes, this file will not be updated.
 		So run gogp tool any times on GoPath is harmless, unless there are indeed changes.
-		So any manualy modification will be restored by tool.Take care of that.`)
+		So any manualy modification will be restored by tool.
+		Take care of that.`)
 
-	cmdline.StringVar(&filePath, "", "filePath", filePath, false, "Path that gogp will work, if not set, it will work on GoPath.")
-	cmdline.BoolVar(&reverseWork, "r", "reverseWork", reverseWork, false, "Reverse work, this mode is used to gen .gp file from a real-go file.\n      If set this flag, the filePath flag must be a .gpg file path related to GoPath.")
+	cmdline.StringVar(&filePath, "", "filePath", filePath, true, "Path that gogp will work, if not set, it will work on GoPath.")
+	cmdline.BoolVar(&reverseWork, "r", "reverseWork", reverseWork, false, "Reverse work, this mode is used to gen .gp file from a real-go file.\nIf set this flag, the filePath flag must be a .gpg file path related to GoPath.")
 	cmdline.StringVar(&codeExt, "e", "codeExt", codeExt, false, "Code file ext name. [.go] is default. [.gp] and [.gpg] is not allowed.")
 	cmdline.Parse()
 
