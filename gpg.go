6b03bbff8c1068c8b796a85b22cafc9bf3b04adb
Ally Dale <vipally@163.com>
2016-11-03 22:46:00 +0800

    error

--- gpg.go
+++ gpg.go
@@ -86,7 +86,7 @@ var (
 	gRemoveProductsOnly = false //remove products only
 
 	gOnceMap map[string]bool //record once processed files
-	gDebug   false           //debug switch
+	gDebug   = false         //debug switch
 )
 
 type gogp_proc_step int
