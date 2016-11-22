4824162da304340da2aa9c9388bb44086ddc0943
Ally Dale <vipally@163.com>
2016-11-22 10:41:15 +0800

    lib

--- sample/sample.go
+++ sample/sample.go
@@ -1,44 +1,17 @@
 package sample
 
-//#GOGP_IGNORE_BEGIN//////////////////////////////GOGPCommentDummyGoFile_BEGIN
-//
-//
-///*   //<----This line can be uncommented to disable all this file, and it doesn't effect to the .gp file
-//	 //If test or change .gp file required, comment it to modify and cmomile as normal go file
-//
-//
-// This is exactly not a real go code file
-// It is used to generate .gp file by gogp tool
-// Real go code file will be generated from .gp file
-//
-//#GOGP_IGNORE_END////////////////////////////////GOGPCommentDummyGoFile
+//#GOGP_FILE_BEGIN 1
 
-//
-//
-//
-//add import here..........
-//
-//
-//
+//#GOGP_REQUIRE(github.com/vipally/gx/stl/gp/fakedef,_)
 
-//#GOGP_IGNORE_BEGIN//////////////////////////////GOGPDummyDefine
-//
-//these defines is used to make sure this dummy go file can be compiled correctlly
-//and they will be removed from real go files
-//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-//
-//add dummy defines here..........
-//
-//
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-//#GOGP_IGNORE_END////////////////////////////////GOGPDummyDefine
+//#GOGP_IGNORE_BEGIN
+//# GOGP_REQUIRE(github.com/vipally/gx/stl/gp/functorcmp,#GOGP_GPGCFG(GOGP_SectionSortSlice))
+//#GOGP_IGNORE_END
 
-//
+////////////////////////////////////////////////////////////////////////////////
+
 //
 //add file body here..........
 //
-//
 
-//#GOGP_IGNORE_BEGIN//////////////////////////////GOGPCommentDummyGoFile
-//*/
-//#GOGP_IGNORE_END////////////////////////////////GOGPCommentDummyGoFile_END
+//#GOGP_FILE_END
