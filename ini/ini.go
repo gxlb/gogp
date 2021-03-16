41dd234f702a2e5a1f3488e3feed9f75dc39e89d
Ally Dale <vipally@gmail.com>
2021-03-16 23:41:16 +0800

    add MIT LICENSE

--- ini/ini.go
+++ ini/ini.go
@@ -1,9 +1,29 @@
-//    CopyRight @Ally Dale 2016
-//    Author  : Ally Dale(vipally@gmail.com)
-//    Blog    : http://blog.csdn.net/vipally
-//    Site    : https://github.com/vipally
-//    Version :   1.0.0
-//    Build at:  [Jan 19 2014  15:28:01]
+// MIT License
+//
+// Copyright (c) 2021 @gxlb
+// Url:
+//     https://github.com/gxlb
+//     https://gitee.com/gxlb
+// AUTHORS:
+//     Ally Dale <vipally@gamil.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 //ini file reader
 package ini
