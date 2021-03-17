8beb7cfea6d90bda1c5bd3747f3c0235a145a332
AllyDale <vipally@gmail.com>
2021-03-17 11:12:05 +0800

    modify text case

--- syntax_test.go
+++ syntax_test.go
@@ -1,18 +1,9 @@
 package gogp
 
 import (
-	"fmt"
-	"regexp"
 	"testing"
 )
 
 func TestAllRegexpSyntax(t *testing.T) {
-
-	gogpExpAll := regexp.MustCompile(fmt.Sprintf("%s", expTxtTodoReplace))
-	txt := `
-	<todoReplace>
-`
-
-	fmt.Println("gogpExpAll match", gogpExpAll.MatchString(txt))
 
 }
