6a92063add428de5527679b774a160226d5c2c9a
Ally Dale <vipally@gmail.com>
2021-03-17 22:13:07 +0800

    export std.regexp APIs

--- vendor/github.com/dlclark/regexp2/regexp_api.go
+++ vendor/github.com/dlclark/regexp2/regexp_api.go
@@ -6,14 +6,22 @@ import (
 	"fmt"
 )
 
+type StdRegexp struct {
+	p *Regexp
+}
+
+func (re *Regexp) StdRegexp() *StdRegexp {
+	return &StdRegexp{p: re}
+}
+
 func makeRepFunc(f func(string) string) MatchEvaluator {
 	return func(m Match) string {
 		return f(m.String())
 	}
 }
 
-func (re *Regexp) ReplaceAllString(input string, repace string) string {
-	rep, err := re.Replace(input, repace, 0, -1)
+func (re *StdRegexp) ReplaceAllString(input string, repace string) string {
+	rep, err := re.p.Replace(input, repace, 0, -1)
 	if err != nil {
 		fmt.Println(err)
 		return input
@@ -21,8 +29,8 @@ func (re *Regexp) ReplaceAllString(input string, repace string) string {
 	return rep
 }
 
-func (re *Regexp) ReplaceAllStringFunc(input string, f func(string) string) string {
-	rep, err := re.ReplaceFunc(input, makeRepFunc(f), 0, -1)
+func (re *StdRegexp) ReplaceAllStringFunc(input string, f func(string) string) string {
+	rep, err := re.p.ReplaceFunc(input, makeRepFunc(f), 0, -1)
 	if err != nil {
 		fmt.Println(err)
 		return input
@@ -30,8 +38,8 @@ func (re *Regexp) ReplaceAllStringFunc(input string, f func(string) string) stri
 	return rep
 }
 
-func (re *Regexp) FindAllStringSubmatch(input string, num int) [][]string {
-	m, err := re.FindStringMatch(input)
+func (re *StdRegexp) FindAllStringSubmatch(input string, num int) [][]string {
+	m, err := re.p.FindStringMatch(input)
 	if err != nil {
 		return nil
 	}
@@ -43,18 +51,18 @@ func (re *Regexp) FindAllStringSubmatch(input string, num int) [][]string {
 			mm = append(mm, (&m.otherGroups[i]).String())
 		}
 		r = append(r, mm)
-		m, err = re.FindNextMatch(m)
+		m, err = re.p.FindNextMatch(m)
 	}
 	return r
 }
 
-func (re *Regexp) IsMatchString(input string) bool {
-	if ok, err := re.MatchString(input); err == nil {
+func (re *StdRegexp) MatchString(input string) bool {
+	if ok, err := re.p.MatchString(input); err == nil {
 		return ok
 	}
 	return false
 }
 
-func (re *Regexp) SubexpNames() []string {
-	return re.GetGroupNames()
+func (re *StdRegexp) SubexpNames() []string {
+	return re.p.GetGroupNames()
 }
